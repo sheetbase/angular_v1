71ac2d9381e37ead5b046ad04a4ff17549821000
Lam Nhan <lamnhan92@gmail.com>
2019-08-15 21:52:27 +0700

    update

--- projects/sheetbase/src/public-api.ts
+++ projects/sheetbase/src/public-api.ts
@@ -2,6 +2,56 @@
  * Public API Surface of sheetbase
  */
 
-export * from './lib/sheetbase.service';
-export * from './lib/sheetbase.component';
-export * from './lib/sheetbase.module';
+export { SheetbaseModule } from './lib/sheetbase.module';
+
+// sheetbase services
+export { SheetbaseService } from './lib/services/sheetbase/sheetbase.service';
+export { ApiService } from './lib/services/api/api.service';
+export { FetchService } from './lib/services/fetch/fetch.service';
+export { LocalstorageService } from './lib/services/localstorage/localstorage.service';
+export { CacheService } from './lib/services/cache/cache.service';
+export { AuthService } from './lib/services/auth/auth.service';
+export { DatabaseService } from './lib/services/database/database.service';
+export { StorageService } from './lib/services/storage/storage.service';
+export { MailService } from './lib/services/mail/mail.service';
+
+// util services
+export { AppService } from './lib/services/app/app.service';
+export { NavService } from './lib/services/nav/nav.service';
+export { NotificationService } from './lib/services/notification/notification.service';
+export { CurrencyService } from './lib/services/currency/currency.service';
+export { DateService } from './lib/services/date/date.service';
+
+// data services
+export { DataCategoryService } from './lib/services/data-category/data-category.service';
+export { DataTagService } from './lib/services/data-tag/data-tag.service';
+export { DataAuthorService } from './lib/services/data-author/data-author.service';
+export { DataPageService } from './lib/services/data-page/data-page.service';
+export { DataPostService } from './lib/services/data-post/data-post.service';
+export { DataThreadService } from './lib/services/data-thread/data-thread.service';
+export { DataUserService } from './lib/services/data-user/data-user.service';
+export { DataProductService } from './lib/services/data-product/data-product.service';
+export { DataOrderService } from './lib/services/data-order/data-order.service';
+export { DataNotificationService } from './lib/services/data-notification/data-notification.service';
+export { DataPromotionService } from './lib/services/data-promotion/data-promotion.service';
+export { DataAudioService } from './lib/services/data-audio/data-audio.service';
+export { DataVideoService } from './lib/services/data-video/data-video.service';
+export { DataBundleService } from './lib/services/data-bundle/data-bundle.service';
+
+// components
+export { OauthPopupComponentModule } from './lib/components/oauth-popup/oauth-popup.module';
+export { OopsComponentModule } from './lib/components/oops/oops.module';
+export { SkeletonComponentModule } from './lib/components/skeleton/skeleton.module';
+
+// pipes
+export { O2aPipeModule } from './lib/pipes/o2a/o2a.module';
+export { FilterPipeModule } from './lib/pipes/filter/filter.module';
+export { SafePipeModule } from './lib/pipes/safe/safe.module';
+export { CurrencyxPipeModule } from './lib/pipes/currencyx/currencyx.module';
+export { DatexPipeModule } from './lib/pipes/datex/datex.module';
+export { AgoPipeModule } from './lib/pipes/ago/ago.module';
+export { LisPipeModule } from './lib/pipes/lis/lis.module';
+
+// misc
+export * from './lib/types';
+export * from './lib/utils';
