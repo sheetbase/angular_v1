c445f9cf3b50e35ce89ec54a1d5d99a05e1a37e0
Lam Nhan <lamnhan92@gmail.com>
2019-08-16 22:29:54 +0700

    update sheetbase configs

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,6 +4,8 @@ import { Router } from '@angular/router';
 import { initializeApp } from '@sheetbase/client/app';
 import '@sheetbase/client/database';
 import '@sheetbase/client/auth';
+import '@sheetbase/client/mail';
+import '@sheetbase/client/storage';
 
 import { SheetbaseService, NavService } from 'sheetbase';
 
@@ -20,37 +22,25 @@ export class AppComponent {
     private navService: NavService,
   ) {
 
-    // sheetbase app
-    const sheetbaseApp = initializeApp({
-      // backend
-      backendUrl: 'https://script.google.com/macros/s/AKfycbzOWB29HowmRo76GWM6feByuwlWfZnGTlR2V2cdw27G7dVIesXON4eSHDm4slmt2GRK/exec',
-      apiKey: '9fF1UxGBqbKgocpT81k6y53UGmmDOPAG',
-      // auth
-      authProviders: {
-        'google.com': {
-          clientId: '719937802037-cdu8pf3n6bk0q0jisappedclh01atgq4.apps.googleusercontent.com'
-        },
-        'facebook.com': {
-          clientId: '276545556215548'
-        }
-      },
-      // database
-      databaseId: '1PrLMrZwOsvOmPLcqUuJA9tjki2ChhRsvEu4rkYKrrwU',
-      databaseGids: {
-        categories: '101',
-        tags: '102',
-        pages: '103',
-        posts: '104',
-        audios: '114',
-        videos: '115',
-        bundles: '116',
-        products: '107',
-        authors: '117'
-      },
-    });
-
     // init app
-    this.sheetbaseService.setApp(sheetbaseApp);
+    this.sheetbaseService.setApp(
+      initializeApp({
+        // backend
+        backendUrl: 'https://script.google.com/macros/s/AKfycbxaiKoJp0w1-0RFoGGFG2CA0XRrunSXjiH5FZDWGUsFSfFWcdlEH9szDIsxOXUC6YsJAw/exec',
+        apiKey: 'RU2h6FREPlzoZo2Itha28uG5toGAk78U',
+        // auth
+        authProviders: {
+          'google.com': {
+            clientId: '719937802037-cdu8pf3n6bk0q0jisappedclh01atgq4.apps.googleusercontent.com'
+          },
+          'facebook.com': {
+            clientId: '276545556215548'
+          }
+        },
+        // database
+        databaseId: '11Owi30p-FHIMH7Z44LUmA0KYiYCkMdUGBS_iApBf_rg',
+      }),
+    );
 
     // init nav
     this.navService.setRouter(this.router);
